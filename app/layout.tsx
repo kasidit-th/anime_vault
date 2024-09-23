795c83824f94ef7a739628ce42f67a1fee769114
Kasidit Thong-on <141108371+kasidit-th@users.noreply.github.com>
2024-09-23 20:03:37 +0700

    .

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+
+import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
+
+import "./globals.css";
+
+const dmSans = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Jhong Jhong Anime",
+  description: "Your favorite anime, all in",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={dmSans.className}>
+        <main className="max-w-7xl mx-auto bg-[#f790e4]">
+          <Hero />
+          {children}
+          <Footer />
+        </main>
+      </body>
+    </html>
+  );
+}
